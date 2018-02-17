13ac09865cc23689f07d7b8441d016860456c945
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-02-17 12:36:52 +0000

    Read me e media
    continuação do programa para calcular a média

--- Calculos_para_alunos/Calculo_Media_Secundario.py
+++ Calculos_para_alunos/Calculo_Media_Secundario.py
@@ -1,5 +1,5 @@
 """
-Calculadora da média do secundário para alunos de ciências e tecnologias do ensino oficial em Portugal.
+Calculadora da média do secundário para alunos do ensino oficial em Portugal.
 Mais opções serão disponibilizadas.
 More in 
   SoloLearn: https://www.sololearn.com/Profile/8198571
@@ -16,14 +16,47 @@ Definir grupos
 """
 Sim_expressions = ("Yes", "Y", "y", "yes", "Sim", "sim", "S", "s", "YES", "SIM")
 Nao_expressions = ("No", "no", "N", "n", "NO", "Não", "não", "NÃO", "Nao", "nao", "nao")
+Expressions_1 = ("1", "(1)")
+Expressions_2 = ("2", "(2)")
+Expressions_3 = ("3", "(3)")
 """
 Menu
 """
 nao_tenho_impressora_mas_se_tivesse_era_isto_que_fazia("Bem vindo à calculadora de médias!\n\n")
-curso = input("É aluno de ciências e tecnologias do ensino oficial em Portugal?\n:")
-if curso in Nao_expressions:
-    tchau("Desculpe, mas o seu curso não está disponível atualmente.\nPor favor tente noutra altura.")
-elif curso in Sim_expressions:
-    nao_tenho_impressora_mas_se_tivesse_era_isto_que_fazia("por editar")
-else:
-    tchau("O que intruduziu não é válido.")
+ter_curso = input("É aluno do ensino oficial em Portugal?\n:")
+while True:
+    if ter_curso in Nao_expressions:
+        tchau("Desculpe, mas o seu curso não está disponível atualmente.\nPor favor tente noutra altura.")
+    elif ter_curso in Sim_expressions:
+        nao_tenho_impressora_mas_se_tivesse_era_isto_que_fazia("\n")
+        break
+    else:
+        print("O que intruduziu não é válido.\n")
+        ter_curso = input("É aluno do ensino oficial em Portugal?\n:")
+"""
+Curso:
+"""
+nao_tenho_impressora_mas_se_tivesse_era_isto_que_fazia("Curso:\n(1) Ciências e tecnologias\n(2) Ciências Socioeconómicas\n(3) Outro")
+input_curso = input(":")
+while True:
+    if input_curso in Expressions_1:
+        escolha_curso = "Ciências e tecnologias"
+        break
+    elif input_curso in Expressions_2:
+        escolha_curso = "Ciências socioeconómicas"
+        break
+    elif input_curso in Expressions_3:
+        print("O calculo da média para o seu curso não é possível de momento.\n")
+        input_curso = input(":")
+    else:
+        print("O que intruduziu não é válido.\n")
+        input_curso = input(":")
+"""
+Disciplinas:
+"""
+
+"""
+Escolha as suas disciplinas.\n\n\n10ª e 11º\n\nPortuguês\n\nFilosofia\n\nMatemática A\n\n
+nao_tenho_impressora_mas_se_tivesse_era_isto_que_fazia("Escolha bianual:\n(1) Matemática A\n(2) História A\n(3) Geografia A")
+input_bianual
+"""
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Multi_Calculator
+
+Diverse calculators written in Python 3.
+Includes.
+    Calculator with basic math functions (+, -, *, /, ^, root).
+    Calculator of perimeters, areas and volumes.
+    Calculator for conversions.
+    Calculator for averages of school grades.
+    Calculator for the study of physics.
