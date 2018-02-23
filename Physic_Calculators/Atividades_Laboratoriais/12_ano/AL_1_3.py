eafe26548b96b819daa382b13687ec86f6881e0a
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-02-23 14:59:06 +0000

    doc
    documentation al 1.3

--- Physic_Calculators/Atividades_Laboratoriais/12_ano/AL_1_3.py
+++ Physic_Calculators/Atividades_Laboratoriais/12_ano/AL_1_3.py
@@ -1,8 +1,6 @@
 while True:
 	while True:
-"""
-Input da massa do carrinho e conversão para Kg.
-"""
+#Input da massa do carrinho e conversão para Kg.
 		massa = input("Insira a massa do carrinho (g):")
 		try:
 			massa = float(massa)
@@ -10,9 +8,7 @@ Input da massa do carrinho e conversão para Kg.
 		except:
 			print("O que inseriu não é válido.")
 	massa_Kg = massa * 0.001
-"""
-Input do comprimento da banda de cor escura e conversão para m.
-"""
+#Input do comprimento da banda de cor escura e conversão para m.
 	while True:
 		Picket_fence = input("Insira o comprimento da faixa escura do picket fence (cm):")
 		try:
@@ -21,9 +17,9 @@ Input do comprimento da banda de cor escura e conversão para m.
 		except:
 			print("O que inseriu não é válido.")
 	Picket_fence_m = Picket_fence * 0.01
-"""
-Input do intervalo de tempo que a célula fotovoltaica 1 esteve interrompida (antes da colisão).
-"""
+
+#Input do intervalo de tempo que a célula fotovoltaica 1 esteve interrompida (antes da colisão).
+
 	while True:
 		Δt_i = input("Insira o primeiro intervalo de tempo (s):")
 		try:
@@ -31,9 +27,7 @@ Input do intervalo de tempo que a célula fotovoltaica 1 esteve interrompida (an
 			break
 		except:
 			print("O que inseriu não é válido.")
-"""
-Input do intervalo de tempo que a célula fotovoltaica 2 esteve interrompida (depois da colisão).
-"""
+#Input do intervalo de tempo que a célula fotovoltaica 2 esteve interrompida (depois da colisão).
 	while True:
 		Δt_f = input("Insira o segundo intervalo de tempo (s):")
 		try:
@@ -41,40 +35,26 @@ Input do intervalo de tempo que a célula fotovoltaica 2 esteve interrompida (de
 			break
 		except:
 			print("O que inseriu não é válido.")
-"""
-Cálculo das velocidades (Δx/Δt).
-"""
+#Cálculo das velocidades (Δx/Δt).
 	v_i = Picket_fence_m / Δt_i
 	v_f = Picket_fence_m / Δt_f
-"""
-Cálculo do momento linear (m*v).
-"""
+#Cálculo do momento linear (m*v).
 	p_i = massa_Kg * v_i
 	p_f = massa_Kg * v_f
-"""
-Cálculo da energia cinética ((1/2)*m*v^2).
-"""
+#Cálculo da energia cinética ((1/2)*m*v^2).
 	E_c_i = 0.5 * massa_Kg * (v_i ** 2)
 	E_c_f = 0.5 * massa_Kg * (v_f ** 2)
-"""
-Cálculo do coeficiente de restituição da velocidade após a colisão com a mola (vf/vi).
-"""
+#Cálculo do coeficiente de restituição da velocidade após a colisão com a mola (vf/vi).
 	Coef_restituicao = v_f / v_i
-"""
-Conversão dos floats em strings.
-"""
+#Conversão dos floats em strings.
 	massa_Kg_str = str(massa_Kg)
 	Picket_fence_m_str = str(Picket_fence_m)
 	Δt_i_str = str(Δt_i)
 	Δt_f_str = str(Δt_f)
-"""
-Menu de confirmação dos inputs.
-"""
+#Menu de confirmação dos inputs.
 	print("\n\nSão estes os valores que pretende usar?\n" + "\nMassa (Kg):" + massa_Kg_str + "\nComprimento da faixa escura (m):" + Picket_fence_m_str + "\n1º intervalo de tempo (s):" + Δt_i_str + "\n2º intervalo de tempo (s):" + Δt_f_str)
 	user_decision_1 = input("\n(sim/não):")
-"""
-Resultados.
-"""
+#Resultados.
 	if user_decision_1 == "sim":
 		v_i_str = str(v_i)
 		v_f_str = str(v_f)
@@ -91,9 +71,7 @@ Resultados.
 			break
 		else:
 			print("O que introduziu não é válido.")
-"""
-Substituição dos inputs.
-"""
+#Substituição dos inputs.
 	elif user_decision_1 == "não":
 		print("\n")
 		continue
