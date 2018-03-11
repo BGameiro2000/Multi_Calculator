be33811ffa4e963b18ccd83331c2b6d3dabefe55
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-11 20:21:57 +0000

    new intro
    ownership

--- Physic_Calculators/Atividades_Laboratoriais/Atividades_laboratoriais_secundário.py
+++ Physic_Calculators/Atividades_Laboratoriais/Atividades_laboratoriais_secundário.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
 """
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro)
+More in 
+  SoloLearn: https://www.sololearn.com/Profile/8198571
+  GitHub: https://github.com/BGameiro76
+  Repl.it: https://repl.it/@B_Gameiro
+  
 Programa para realizar os calculos da atividade laboratoriais de Física do Secundário
 """
 from sys import exit as end
 Sim_expressions = ["Yes", "Y", "y", "yes", "Sim", "sim", "S", "s", "YES", "SIM"]
 Nao_expressions = ["No", "no", "N", "n", "NO", "Não", "não", "NÃO", "Nao", "nao", "nao"]
 #Welcome
-run_program = input("\n\n\n\n\nBem Vindo às atividades laboratoriais de Física do secundário!\nEste programa destina-se a auxiliar-te nas atividades experimentais de 10º, 11º e 12º ano do secundário.\n\nEste programa contem informação importante relativa com as mesmas.\nNomeadamente considerações, aproximações, cálculos, esquemas e objetivos das mesmas.\n\nApenas tens de inserir a informação necessária sempre que vires um dos seguntes sinais: \"ᨌ \" ou \"=\"\n\n\nVamos começar?\nᨌ ")
+run_program = input("\n\n\n\n\nBem Vindo às atividades laboratoriais de Física do secundário!\nEste programa destina-se a auxiliar-te nas atividades experimentais de 10º, 11º e 12º ano do secundário.\n\nEste programa contem informação importante relativa com as mesmas.\nNomeadamente considerações, aproximações, cálculos, esquemas e objetivos das mesmas.\n\nApenas tens de inserir a informação necessária sempre que vires um dos seguntes sinais: \"𠂭 \" ou \"=\"\n\n\nVamos começar?\n𠂭 ")
 if run_program in Sim_expressions:
     print("\nÓtimo!\nVamos começar.\n")
 elif run_program in Nao_expressions:
@@ -28,11 +36,11 @@ def atividade12(x, y):
 
 #Escolher ano
 while True:
-    ano = input("Qual é o ano da atividade experimental que quer realizar?\nᨌ ")
+    ano = input("Qual é o ano da atividade experimental que quer realizar?\n𠂭 ")
     if ano == "10" or ano == "10º" or ano == "décimo"  or ano == "decimo" or ano == "dez":
         ano = "10º"
         while True:
-            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\nᨌ ")
+            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\n𠂭 ")
             if ano_verificar in Sim_expressions:
                 print("A iniciar 10º ano...")
             elif ano_verificar in Nao_expressions:
@@ -42,7 +50,7 @@ while True:
     elif ano == "11" or ano == "11º" or ano == "décimo primeiro"  or ano == "decimo primeiro" or ano == "onze":
         ano = "11º"
         while True:
-            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\nᨌ ")
+            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\n𠂭 ")
             if ano_verificar in Sim_expressions:
                 print("A iniciar 11º ano...")
             elif ano_verificar in Nao_expressions:
@@ -52,7 +60,7 @@ while True:
     elif ano == "12" or ano == "12º" or ano == "décimo segundo"  or ano == "decimo segundo" or ano == "doze":
         ano = "12º"
         while True:
-            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\nᨌ ")
+            ano_verificar = input("É este o ano que deseja utilizar (" + ano + " ano)?\n𠂭 ")
             if ano_verificar in Sim_expressions:
                 print("A iniciar 12º ano...")
             elif ano_verificar in Nao_expressions:
