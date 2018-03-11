00c1ffdccfe4eb13e28ff40d5dbb9d0277fa62e7
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-11 20:22:06 +0000

    new intro
    ownership

--- Physic_Calculators/Lancamentos.py
+++ Physic_Calculators/Lancamentos.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """
-Calculadora de lançamentos.
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro)
 More in 
   SoloLearn: https://www.sololearn.com/Profile/8198571
   GitHub: https://github.com/BGameiro76
   Repl.it: https://repl.it/@B_Gameiro
+
+Calculadora de lançamentos.
 """
