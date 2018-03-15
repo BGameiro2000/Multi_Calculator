9f58d00b5930efd3169268ad3c111b9aff026c50
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-15 23:03:54 +0000

    .
    .

--- Math_Calculators/Perimeter_Area_Volume_Calculator.py
+++ Math_Calculators/Perimeter_Area_Volume_Calculator.py
@@ -13,7 +13,7 @@ Supports perimeters, areas and volumes.
 """
 Menu.
 """
-print("Options:\n\n\nEnter what you want to calculate (perimeter, area or volume) and the the geometric figure/solid you want.\n\n\nNote:\n\n- The geometric figures supported are ./n- The solids supported are .")
+print("Options:\n\n\nEnter what you want to calculate (perimeter, area or volume) and the the geometric figure/solid you want.\n\n\nNote:\n\n- The geometric figures supported are .\n- The solids supported are .")
 """
 Defining groups of inputs.
 """
@@ -35,15 +35,15 @@ while True:
   user_input_operation = input("Enter an operation : ")
 #Calculation
   if user_input_operation in Perimeter_expressions:
-    geometric_figure = input("Choose a geometric figure./n(1) Regular geometric figures (equal sides)/n(2) Rectangle/n(3) Triangle/n(4) Circle/n(5)/nGeometric figure:")
+    geometric_figure = input("Choose a geometric figure.\n(1) Regular geometric figures (equal sides)\n(2) Rectangle\n(3) Triangle\n(4) Circle\n(5) Geometric figure:")
     if geometric_figure in Expressions_1:
-      Number_of_sides = input("How many sides?/n:")
+      Number_of_sides = input("How many sides?\n:")
       Number_of_sides_float = float(Number_of_sides)
       if Number_of_sides_float > 2 and Number_of_sides_float != 4:
         Measurement_Sides = float(input("Measurement of the sides:"))
         print(Number_of_sides_float * Measurement_Sides)
       elif Number_of_sides_float == 4:
-        Calculation_option_Square = input("What do you want to use as measurement?/n(1) Side/n(2) Diagonal/nOption:")
+        Calculation_option_Square = input("What do you want to use as measurement?\n(1) Side\n(2) Diagonal\nOption:")
         if Calculation_option in Expressions_1:
           Square_Side = input("Measurement:")
           print(4 * Square_Side)
@@ -64,14 +64,14 @@ while True:
       Triangle_Side3 = float(input("Measurement of side 3:"))
       print(Triangle_Side1 + Triangle_Side2 + Triangle_Side3)
     elif geometric_figure in Expressions_4:
-      Use_Pi = input("Use Pi or define it?/n(1) Use Pi/n(2) Use custom Pi/n:")
+      Use_Pi = input("Use Pi or define it?\n(1) Use Pi\n(2) Use custom Pi\n:")
       if Use_Pi in Expressions_1:
         pi = pie
       elif Use_Pi in Expressions_2:
         pi = float(input("Custom Pi:"))
       else:
         print("Unknown input")
-      Calculation_option_Circle = input("What do you want to use as measurement?/n(1) Radius/n(2) Diameter/nOption:")
+      Calculation_option_Circle = input("What do you want to use as measurement?\n(1) Radius\n(2) Diameter\nOption:")
       if Calculation_option_Circle in Expressions_1:
         Diameter_Circle = 2 * float(input("Measurement of the radius:"))
       elif Calculation_option_Circle not in Expressions_1 and Calculation_option_Circle not in Expressions_2:
@@ -80,11 +80,23 @@ while True:
         Diameter_Circle = float(input("Measurement of the diameter:"))
       print (pi * Diameter_Circle)
     elif geometric_figure in Expressions_5:
+      Number_of_sides_fig = input("How many sides?\n:")
+      Number_of_sides_fig_int = int(Number_of_sides_fig)
+      sides = list(range(0,Number_of_sides_fig_int))
+      print(sides)
+      for i in range(0, Number_of_sides_fig_int, 1):
+      	sides[i] = float(input("Side " + str(i+1) + ": "))
+      	i += 1
+      print(sides)
+      perimeter_fig = 0
+      for i in sides:
+      	perimeter_fig += i
+      print(perimeter_fig)
     else:
       print("That's not a supported geometric figure.")
   elif user_input_operation in Area_expressions:
-    input("Choose a geometric figure :/n(1)/n(2)/n(3)/n(4)/n(5)")
+    input("Choose a geometric figure :\n(1)\n(2)\n(3)\n(4)\n(5)")
   elif user_input_operation in Volume_expressions:
-    input("Choose a solid :/n(1)/n(2)/n(3)/n(4)/n(5)")
+    input("Choose a solid :\n(1)\n(2)\n(3)\n(4)\n(5)")
   else:
     print("Unknown input")
