545ea8cc2bf88d863978c233ab220fafce16f80d
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-03-15 23:04:15 +0000

    .
    .

---
+++ Physic_Calculators/Atividades_Laboratoriais/12_ano/AL_1_3_.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro) e Fran
+More in 
+  SoloLearn: https://www.sololearn.com/Profile/8198571
+  GitHub: https://github.com/BGameiro76
+  Repl.it: https://repl.it/@B_Gameiro
+
+Função para realizar a atividade experimental A.L.1.3.
+"""
+
+def AL13(funcao)
