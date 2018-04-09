9670502a50c4feae4c1d94a1f735fd35966cdc40
BGameiro76 <33392539+BGameiro76@users.noreply.github.com>
2018-04-09 15:27:57 +0100

    first commit
    env & info

---
+++ Physic_Calculators/Atividades_Laboratoriais/Current Version/AtividadesLaboratoriais.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This code is under Apache License 2.0
+Written by B_Gameiro (Bernardo Bernardino Gameiro)
+More in 
+    SoloLearn: https://www.sololearn.com/Profile/8198571
+    GitHub: https://github.com/BGameiro76
+    Repl.it: https://repl.it/@B_Gameiro
+
+GUI para as atividades laboratoriais de Física do secundário em Python 3 e Tkinter
+"""
